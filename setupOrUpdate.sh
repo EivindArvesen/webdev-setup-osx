d4d6c0712b04941e700d42130d3108f53925dc33
Eivind Arvesen <eivind.arvesen@gmail.com>
2016-09-10 11:10:40 +0200

    Set screenshot dir to downloads

--- setupOrUpdate.sh
+++ setupOrUpdate.sh
@@ -129,6 +129,9 @@ defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebK
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
+defaults write com.apple.screencapture location ~/Downloads
+killall SystemUIServer
+
 #killall Dock
 #killall Finder
 
