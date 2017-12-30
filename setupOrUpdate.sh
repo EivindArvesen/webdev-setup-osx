3946cf7c4d9b6bc9db31fb8bcc20dc4f3077085a
Eivind Arvesen <eivind.arvesen@gmail.com>
2017-12-30 17:39:07 +0100

    Update default OS settings

--- setupOrUpdate.sh
+++ setupOrUpdate.sh
@@ -123,15 +123,18 @@ defaults write com.apple.Safari HomePage -string "www.google.com"
 # Prevent Safari from opening ‘safe’ files automatically after downloading
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 # Enable Safari’s debug menu
-#defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
+# Save screenshots to Downloads folder
 defaults write com.apple.screencapture location ~/Downloads
+# Set local disk as default save target
+defaults write -g NSDocumentSaveNewDocumentsToCloud -bool false
+
 killall SystemUIServer
 
 #killall Dock
